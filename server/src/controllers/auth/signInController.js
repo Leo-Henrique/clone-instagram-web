e757ea43d26abce151d3b27944b0c193e90d8398
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-04-18 11:42:58 -0300

    refactor: update sign in controller

--- server/src/controllers/auth/signInController.js
+++ server/src/controllers/auth/signInController.js
@@ -4,31 +4,31 @@ import { error, requiredFields } from "../../utils/helpers/validations.js";
 import generateToken from "../../utils/helpers/token.js";
 
 export default async function signIn(req, res) {
-    const { user, password } = req.body;
+    const { user: userIdentify, password } = req.body;
     const values = Object.values(req.body);
 
     try {
-        let userData;
-
         if (requiredFields(values, res)) return requiredFields(values, res);
 
-        const userExists = await Promise.all([
-            User.findOne({ email: user }).select("+password"),
-            User.findOne({ username: user }).select("+password"),
+        let user = await Promise.all([
+            User.findOne({ email: userIdentify }).select("+password"),
+            User.findOne({ username: userIdentify }).select("+password"),
         ]);
 
-        if (!userExists.filter(item => !!item).length)
+        const userExists = user.filter(item => !!item);
+
+        if (!userExists.length)
             return error("Usuário não encontrado.", 400, res);
 
-        userData = userExists[0];
+        user = userExists[0];
 
-        if (!(await bcrypt.compare(password, userData.password)))
+        if (!(await bcrypt.compare(password, user.password)))
             return error("Senha inválida.", 400, res);
 
-        userData.password = undefined;
+        user.password = undefined;
         res.send({
-            user: userData,
-            token: generateToken(userData.id)
+            user: user,
+            token: generateToken(user.id)
         });
     } catch (err) {
         return error(
