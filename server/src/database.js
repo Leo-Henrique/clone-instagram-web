37a3c5c969b1df0d69770c3dc5772e0e22dab6eb
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-04-18 16:35:33 -0300

    chore: change database name

--- server/src/database.js
+++ server/src/database.js
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
 export default function run() {
     const username = process.env.DB_USER;
     const password = process.env.DB_PASS;
+    const name = process.env.DB_NAME;
 
-    const uri = `mongodb+srv://${username}:${password}@clone-instagram-web.vw7sksb.mongodb.net/?retryWrites=true&w=majority`;
+    const uri = `mongodb+srv://${username}:${password}@clone-instagram-web.vw7sksb.mongodb.net/${name}?retryWrites=true&w=majority`;
 
     mongoose.connect(uri);
 }
