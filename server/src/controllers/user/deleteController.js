af312e92f87dcf99e056a7fb488c73e1343271ca
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-04-20 09:08:20 -0300

    feat: create controller for delete user

---
+++ server/src/controllers/user/deleteController.js
@@ -0,0 +1,31 @@
+import User from "../../models/userModel.js";
+import { error } from "../../utils/helpers/validations.js";
+import bcrypt from "bcryptjs";
+
+export default async function deleteUser(req, res) {
+    const { password } = req.body;
+
+    try {
+        if (!password)
+            return error(
+                "Você deve fornecer sua senha para excluir sua conta.",
+                400,
+                res
+            );
+        
+        const user = await User.findById(req.userId).select("+password");
+
+        if (!(await bcrypt.compare(password, user.password)))
+            return error("Sua senha está incorreta.", 400, res);
+
+        await User.findByIdAndDelete(req.userId);
+
+        res.send({ success: "Sua conta foi excluída."})
+    } catch (err) {
+        return error(
+            "Não foi possível excluir sua conta. Tente novamente mais tarde.",
+            500,
+            res
+        );
+    }
+}
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -5,6 +5,7 @@ import {
     updatePassword,
 } from "../controllers/user/updateController.js";
 import { follow, unfollow } from "../controllers/user/followController.js";
+import deleteUser from "../controllers/user/deleteController.js";
 
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router.patch("/", updateInfos);
 router.patch("/password", updatePassword);
 router.post("/follow", follow);
 router.delete("/unfollow", unfollow);
+router.delete("/", deleteUser);
 
 export default router;
