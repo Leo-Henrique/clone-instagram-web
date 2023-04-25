46ee850533a375d755ba761bce6ffbf7ab7f6b1c
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-04-25 15:28:37 -0300

    feat: create controller for read posts

---
+++ server/src/controllers/post/readController.js
@@ -0,0 +1,37 @@
+import Post from "../../models/postModel.js";
+import User from "../../models/userModel.js"
+import { error } from "../../utils/helpers/validations.js";
+
+export const getPosts = async (req, res) => {
+    const { items, collection, username } = req.query;
+
+    try {
+        const user = await User.findById(req.userId);
+        const populate = "user media.persons.user";
+        const [allPosts, feed] = await Promise.all([
+            Post.find().populate(populate),
+            Post.find({ user: { $in: user.following } }).populate(populate)
+        ]);
+        const posts = username 
+            ? allPosts.filter(({ user }) => 
+                user.username === username.toLowerCase()
+            )
+            : allPosts.filter(({ user }) => 
+                user.id === req.userId
+            ).concat(feed);
+
+        posts.sort((a, b) => b.createdAt - a.createdAt);
+
+        const getCollection = () => {
+            const endIndex = items * collection;
+
+            return posts.filter((post, index) => 
+                index >= endIndex - items && index < endIndex
+            );
+        }
+
+        return res.send(items ? getCollection() : posts);
+    } catch (err) {
+        return error("Não foi possível carregar as publicações.", 500, res);
+    }
+}
--- server/src/routes/postRoutes.js
+++ server/src/routes/postRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import createPost from "../controllers/post/createController.js";
 import updatePost from "../controllers/post/updateController.js";
+import { getPosts } from "../controllers/post/readController.js";
 
 const router = express.Router();
 
 router.use(authMiddleware);
 router.post("/", createPost);
 router.patch("/:postId", updatePost);
+router.get("/", getPosts);
 
 export default router;
