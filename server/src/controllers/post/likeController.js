d6255636d8edbd87648fc1189d6fbc0437c1e168
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-04-29 16:50:49 -0300

    feat: create like controller for posts

---
+++ server/src/controllers/post/likeController.js
@@ -0,0 +1,57 @@
+import Post from "../../models/postModel.js";
+import { error } from "../../utils/helpers/validations.js";
+
+export const like = async (req, res) => {
+    const { postId } = req.params;
+    const { userId } = req;
+
+    try {
+        const post = await Post.findById(postId);
+        const hasLike = post.likes.some(id => id.toString() === userId);
+
+        if (hasLike) return error("Você já curtiu a publicação.", 400, res);
+        else post.likes.push(userId);
+
+        post.save();
+        res.send();
+    } catch (err) {
+        return error("Não foi possível curtir a publicação. Tente novamente.", 500, res);
+    }
+};
+
+export const getLikes = async (req, res) => {
+    const { postId } = req.params;
+
+    try {
+        const post = await Post.findById(postId).populate("likes");
+
+        post.likes.sort(() => 0.5 - Math.random());
+        res.send(post.likes);
+    } catch (err) {
+        return error("Não foi possível obter as curtidas.", 500, res);
+    }
+}
+
+export const unlike = async (req, res) => {
+    const { postId } = req.params;
+    const { userId } = req;
+
+    try {
+        const post = await Post.findById(postId);
+        const index = post.likes.findIndex(id => 
+            id.toString() === userId
+        );
+
+        if (index === -1) throw new Error();
+        else post.likes.splice(index, 1);
+
+        post.save();
+        res.send();
+    } catch (err) {
+        return error(
+            "Não foi possível deixar de curtir a publicação. Tente novamente.",
+            500,
+            res
+        );
+    }
+};
--- server/src/routes/postRoutes.js
+++ server/src/routes/postRoutes.js
@@ -4,6 +4,7 @@ import createPost from "../controllers/post/createController.js";
 import updatePost from "../controllers/post/updateController.js";
 import { getPost, getPosts } from "../controllers/post/readController.js";
 import deletePost from "../controllers/post/deleteController.js";
+import { getLikes, like, unlike } from "../controllers/post/likeController.js";
 
 const router = express.Router();
 
@@ -13,7 +14,10 @@ router.get("/", getPosts);
 router.use(authMiddleware);
 
 router.post("/", createPost);
+router.post("/likes/:postId", like);
+router.get("/likes/:postId", getLikes);
 router.patch("/:postId", updatePost);
 router.delete("/:postId", deletePost);
+router.delete("/likes/:postId", unlike);
 
 export default router;
