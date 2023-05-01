6c4da41c6330f33d6cfc89142efa05ef3019fbd3
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-05-01 14:59:01 -0300

    feat: create controller for like comment

---
+++ server/src/controllers/comment/likeController.js
@@ -0,0 +1,94 @@
+import Comment from "../../models/commentModel.js";
+import { error } from "../../utils/helpers/validations.js";
+
+const getReply = async (id) => {
+    const parent = await Comment.findOne({ replies: {
+        $elemMatch: { _id: id }
+    } });
+    const comment = parent.replies.find(reply => reply.id.toString() === id);
+
+    return { parent, comment };
+}
+
+export const like = async (req, res) => {
+    const { commentId } = req.params;
+
+    try {
+        let comment = await Comment.findById(commentId);
+        let doc = comment;
+
+        if (!comment) {
+            const reply = await getReply(commentId);
+
+            comment = reply.comment;
+            doc = reply.parent;
+        }
+
+        const hasLike = comment.likes.some(id => id.toString() === req.userId);
+
+        if (hasLike) return error("Você já curtiu o comentário.", 400, res);
+
+        comment.likes.push(req.userId)
+        doc.save();
+        res.send();
+    } catch (err) {
+        return error("Não foi possível curtir o comentário. Tente novamente.", 500, res);
+    }
+};
+
+export const getLikes = async (req, res) => {
+    const { commentId } = req.params;
+
+    try {
+        let comment = await Comment.findById(commentId);
+        let doc = comment;
+
+        if (!comment) {
+            const reply = await getReply(commentId);
+
+            comment = reply.comment;
+            doc = reply.parent;
+            await doc.populate("replies.likes");
+        } else await doc.populate("likes");
+
+        const { likes } = comment;
+
+        likes.sort(() => 0.5 - Math.random());
+        res.send(likes);
+    } catch (err) {
+        return error("Não foi possível obter as curtidas.", 500, res);
+    }
+}
+
+export const unlike = async (req, res) => {
+    const { commentId } = req.params;
+
+    try {
+        let comment = await Comment.findById(commentId);
+        let doc = comment;
+
+        if (!comment) {
+            const reply = await getReply(commentId);
+
+            comment = reply.comment;
+            doc = reply.parent;
+        }
+
+        const { likes } = comment;
+        const likeIndex = likes.findIndex(id =>
+            id.toString() === req.userId
+        );
+
+        if (likeIndex === -1) throw new Error();
+        else likes.splice(likeIndex, 1);
+
+        doc.save();
+        res.send();
+    } catch (err) {
+        return error(
+            "Não foi possível deixar de curtir o comentário. Tente novamente.", 
+            500, 
+            res
+        );
+    }
+}
--- server/src/routes/commentRoutes.js
+++ server/src/routes/commentRoutes.js
@@ -1,10 +1,21 @@
 import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import createComment from "../controllers/comment/createController.js";
+import { getLikes, like, unlike } from "../controllers/comment/likeController.js";
+import Comment from "../models/commentModel.js";
 
 const router = express.Router();
 
 router.use(authMiddleware);
 router.post("/:postId", createComment);
+router.post("/likes/:commentId", like);
+router.get("/likes/:commentId", getLikes);
+router.delete("/likes/:commentId", unlike);
+
+router.get("/", async (req, res) => {
+    const comments = await Comment.find();
+
+    res.send(comments)
+})
 
 export default router;
