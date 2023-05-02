fad31b997a539599f01ab704a5f1d8e4ea84a3a6
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-05-02 15:54:45 -0300

    feat: populate comments infos in posts

--- server/src/controllers/post/readController.js
+++ server/src/controllers/post/readController.js
@@ -5,13 +5,17 @@ import { error } from "../../utils/helpers/validations.js";
 import filteredContent from "../../utils/helpers/filteredContent.js";
 
 const reorder = (posts) => posts.sort((a, b) => b.createdAt - a.createdAt);
+const reorderComments = (comments) => comments.sort((a, b) => b.likes.length - a.likes.length);
 
 export const getPost = async (req, res) => {
     const { postId } = req.params;
 
     try {
-        const post = await Post.findById(postId).populate("user");
-        
+        const post = await Post.findById(postId)
+            .populate("user comments")
+            .populate({ path: "comments", populate: { path: "user" } });
+
+        reorderComments(post.comments);
         return res.send(post);
     } catch (err) {
         return error("Não foi possível carregar a publicação.", 500, res);
@@ -44,12 +48,19 @@ export const getPosts = async (req, res) => {
             const feed = await Post.find({ 
                 ...(getReels || { user: { $in: [req.userId, ...user.following] } }),
                 ...(getReels && { isReel: true })
-            }).populate("user media.persons.user");
-            
+            })
+                .populate("user media.persons.user")
+                .populate({ path: "comments", populate: { path: "user" } });
+
+            feed.forEach(post => {
+                reorderComments(post.comments);
+                post.comments = post.comments.filter((item, index) => index < 2);
+            });
             reorder(feed);
             res.send(filteredContent(feed, req.query));
         }
     } catch (err) {
+        console.log(err)
         return error("Não foi possível carregar as publicações.", 500, res);
     }
 }
