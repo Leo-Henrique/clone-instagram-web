d09c48667f1e03a854fd0241020a99a3291c87ae
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-05-18 12:22:34 -0300

    feat: Disable CORS on API for frontend localhost

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import routes from "./routes/routes.js";
 import database from "./database.js";
+import cors from "cors";
 
 const app = express();
 
 dotenv.config();
 database();
+app.use(cors({ origin: process.env.ALLOW_ACCESS }));
 app.use(express.json());
 app.use("/api", routes);
 app.listen(3000);
