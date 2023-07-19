4d11ddf1068ace2300b8ae77bdea635d3ab33c56
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-07-19 13:17:23 -0300

    build: update vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,5 +5,6 @@ import viteSvgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), viteSvgr({ exportAsDefault: true })],
+    base: "./",
     server: { hmr: { overlayDelay: 3000 } },
 });
